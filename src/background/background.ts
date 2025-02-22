a220fd415ba993d641833f329882cc7ddfab15fd
tndus77 <tndus77@sookmyung.ac.kr>
2025-02-23 02:26:18 +0900

    feature: 알림 기능 추가 (background <-> content)

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,53 @@
-// TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-})
+import { getStoredCities } from '../utils/storage';
+
+const OPEN_WEAHTER_API_KEY = 'e1aeeb160e946cc9bc8e660be59017b9';
+let lastWeather: string | null = 'Rainy';
+
+const fetchWeather = async () => {
+	try {
+		const cities = await getStoredCities();
+		if (!cities) return;
+
+		for (const city of cities) {
+			const res = await fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEAHTER_API_KEY}`
+			);
+
+			const data = await res.json();
+
+			if (data.weather) {
+				const currentWeather = data.weather[0].main;
+				// 이전 날씨와 비
+				if (lastWeather && lastWeather !== currentWeather) {
+					chrome.notifications.create({
+						type: 'basic',
+						iconUrl: 'icon.png',
+						title: '날씨 변경 알림',
+						message: `${city}의 날씨가 '${lastWeather}' -> '${currentWeather}'로 변경되었습니다.`,
+					});
+
+					// content로 전달
+					chrome.runtime.sendMessage({
+						type: 'WEATHER_UPDATE',
+						weather: currentWeather,
+					});
+				}
+
+				lastWeather = currentWeather;
+			}
+		}
+	} catch (e) {
+		console.log('e', e);
+	}
+};
+
+// 일정 주기로 날씨 체크
+chrome.alarms.create('weatherCheck', { periodInMinutes: 0.2 });
+chrome.alarms.onAlarm.addListener((alarm) => {
+	if (alarm.name === 'weatherCheck') {
+		fetchWeather();
+	}
+});
+
+// 초기 실행
+fetchWeather();
